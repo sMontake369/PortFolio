727c742af30b92349ff739b8600a83313556c40a
sMontake369 <s24011@g.metro-cit.ac.jp>
2024-05-27 07:15:50 +0900

    commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,32 @@
-export default function login()
+import React, {FC} from "react";
+import { NextPage } from 'next';
+import "./login.css";
+
+const HomePage: NextPage = () => 
 {
-    return <h1>Login</h1> 
+    return (
+        <div className="login-header">
+            <div className="login">
+                <FormItem string1="メールアドレス" string2="パスワード" />
+            </div>
+        </div>
+    )
 }
+
+interface LoginProps {
+    string1: string;
+    string2: string;
+}
+
+const FormItem: FC<LoginProps> = ({ string1, string2 }) => 
+{
+    return (
+        <div>
+            <h1>マイページログイン</h1>
+            <p>{string1}<input type="text" /></p><p>{string2}<input type="text" /></p>
+            <button className="primary-btn">ログイン</button> 
+        </div>
+    );
+};
+
+export default HomePage;
