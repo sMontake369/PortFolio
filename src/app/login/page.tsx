8130f8ecbd43d40df319ef15a8326c1274d8e729
sMontake369 <s24011@g.metro-cit.ac.jp>
2024-09-20 01:12:17 +0900

    update login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,36 +1,77 @@
-import React, {FC} from "react";
-import { NextPage } from 'next';
-import "./login.css";
-import NavBar from "../components/header";
+'use client';
 
-const HomePage: NextPage = () => 
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import React, {FC} from "react";
+import NavBar from "../components/header";
+import styles from "./login.module.css";
+
+type LoginProps = {
+    email: string;
+    password: string;
+}
+
+export default function Login()
 {
+    const { register, handleSubmit, formState: { errors, isSubmitting },} = useForm<LoginProps>();
+    const [error, setError] = useState('');
+
     return (
         <>
-            <NavBar /> 
-            <div className="login-header">
-                <div className="login">
-                    <FormItem string1="メールアドレス" string2="パスワード" />
+            <NavBar />
+            <div className={styles.loginHeader}>
+                <div className={styles.login}>
+                <form>
+                    <h1>マイページログイン</h1>
+                    {errors.email && errors.email.message}
+                    <p>{error}</p>
+                    <p>
+                        メールアドレス
+                        <input
+                            id='email'
+                            type="text"
+                        />
+                    </p>
+                    <p>
+                        パスワード
+                        <input 
+                        id="pass" 
+                        type="password" 
+                        />
+                        <button 
+                            id='showPass'
+                            onClick={showPassword}>
+                            表示
+                        </button>
+                    </p>
+                    <button 
+                        className={styles.primaryBtn} 
+                        onClick={tryLogin}>
+                        ログイン
+                    </button>
+                </form>
                 </div>
             </div>
         </>
     )
 }
 
-interface LoginProps {
-    string1: string;
-    string2: string;
+function showPassword(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    const password = document.querySelector('input[id="pass"]');
+    if (password) {
+        if (password.getAttribute('type') === 'password') {
+            password.setAttribute('type', 'text');
+        } else {
+            password.setAttribute('type', 'password');
+        }
+    }
 }
 
-const FormItem: FC<LoginProps> = ({ string1, string2 }) => 
-{
-    return (
-        <div>
-            <h1>マイページログイン</h1>
-            <p>{string1}<input type="text" /></p><p>{string2}<input type="text" /></p>
-            <button className="primary-btn">ログイン</button> 
-        </div>
-    );
-};
+function tryLogin(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    const email = document.querySelector('input[type="text"]') as HTMLInputElement | null;
+    const password = document.querySelector('input[id="pass"]') as HTMLInputElement | null;
 
-export default HomePage;
+    if (email && password) alert(`Email: ${email.value}, Password: ${password.value}`);
+}
